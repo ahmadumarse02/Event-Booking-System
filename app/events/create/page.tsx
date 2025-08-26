6af5053fd06a9f25661db22d3cda9bb84797b0a7
Ahmad Umar <ahmadumarse02@gmail.com>
2025-08-26 16:27:03 +0500

    new

--- app/events/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EventForm } from "@/components/event-form"
-
-export default function CreateEventPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <EventForm mode="create" />
-    </div>
-  )
-}
